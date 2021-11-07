5ff9d830dd46873b24e3cb218007e924a5711925
silhavyj <jakub.sil@seznam.cz>
2021-11-07 11:13:47 +0100

    Added logging predefined user's credential when the application starts

--- src/main/java/cz/zcu/kiv/pia/silhavyj/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/cz/zcu/kiv/pia/silhavyj/socialnetwork/SocialNetworkApplication.java
@@ -4,6 +4,8 @@ import cz.zcu.kiv.pia.silhavyj.socialnetwork.config.AppConfiguration;
 import cz.zcu.kiv.pia.silhavyj.socialnetwork.model.user.User;
 import cz.zcu.kiv.pia.silhavyj.socialnetwork.service.user.IUserService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class SocialNetworkApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(SocialNetworkApplication.class);
 
 	private final AppConfiguration appConfiguration;
 	private final IUserService userService;
@@ -30,6 +34,7 @@ public class SocialNetworkApplication implements CommandLineRunner {
 		appConfiguration.getUsers().stream()
 				.forEach(defaultUserConfig -> {
 					User user = defaultUserConfig.buildUser(); // create User
+					logger.info(user.toString());		       // log user's credentials
 					userService.encryptUserPassword(user);	   // encrypt their password
 					userService.saveUser(user);				   // store them in the database
 				});

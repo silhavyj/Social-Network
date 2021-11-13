d4c5278326c85495ea05ec6570571f4b2af9700e
silhavyj <jakub.sil@seznam.cz>
2021-11-13 15:21:12 +0100

    Created post entity

--- src/main/java/cz/zcu/kiv/pia/silhavyj/socialnetwork/model/post/Post.java
+++ src/main/java/cz/zcu/kiv/pia/silhavyj/socialnetwork/model/post/Post.java
@@ -1,4 +1,32 @@
 package cz.zcu.kiv.pia.silhavyj.socialnetwork.model.post;
 
+import cz.zcu.kiv.pia.silhavyj.socialnetwork.model.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private PostType postType;
+
+    @Column(columnDefinition="TEXT")
+    String content;
+
+    private LocalDateTime postedAt;
 }

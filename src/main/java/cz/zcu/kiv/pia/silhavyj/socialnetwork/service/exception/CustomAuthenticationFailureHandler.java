17601bd4a1fe9a024a028fd6301051b527231066
silhavyj <jakub.sil@seznam.cz>
2021-11-27 09:32:38 +0100

    Added comments to the service.exception package

--- src/main/java/cz/zcu/kiv/pia/silhavyj/socialnetwork/service/exception/CustomAuthenticationFailureHandler.java
+++ src/main/java/cz/zcu/kiv/pia/silhavyj/socialnetwork/service/exception/CustomAuthenticationFailureHandler.java
@@ -11,22 +11,42 @@ import java.io.IOException;
 
 import static cz.zcu.kiv.pia.silhavyj.socialnetwork.constant.RegistrationConstants.*;
 
+/***
+ * Custom authentication failure handler. This class is used to catch any authentication
+ * error that may occur and return an appropriate response as to what has gone wrong.
+ * For example, when the user enters invalid credentials, or when they try to log in
+ * while their account has not been activated yet.
+ *
+ * @author Jakub Silhavy (A21N0072P)
+ */
 @Service
 public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
+    /*** default parameter sent back to the client when an error occurs */
     private static final String DEFAULT_ERR_URL = "/sign-in?error=";
 
+    /***
+     * This method gets triggered when a user fails to authenticate.
+     * @param request HTTP request sent from the client to the server
+     * @param response HTTP response sent from the server back to the client
+     * @param exception instance of AuthenticationException
+     * @throws IOException
+     * @throws ServletException
+     */
     @Override
     public void onAuthenticationFailure(final HttpServletRequest request,
                                         final HttpServletResponse response,
                                         final AuthenticationException exception) throws IOException, ServletException {
 
+        // Set the default error message - invalid credentials.
         String errorMsg = INVALID_CREDENTIALS_ERR_MSG;
 
+        // If there is another error message, sent that one to the user first.
         if (exception.getMessage().equalsIgnoreCase(LOCKED_ACCOUNT_FLAG)) {
             errorMsg = LOCKED_ACCOUNT_ERR_MSG;
         }
 
+        // Set the error URL and return it back to the client
         setDefaultFailureUrl(DEFAULT_ERR_URL + errorMsg);
         super.onAuthenticationFailure(request, response, exception);
     }
